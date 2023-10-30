0154b3c0f4352f190e951f84c92f221df4fb386a
TusharJagi <tushar.j@fnp.com>
2023-10-30 14:31:46 +0530

    top background component

---
+++ Components/Background/Top.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "@/styles/components/Background/top.module.scss";
+
+const TopBackGround = (): JSX.Element => {
+  return <div className={styles.top_background_wrapper}></div>;
+};
+
+export default TopBackGround;
