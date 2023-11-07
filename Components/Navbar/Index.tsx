7d6e428ebb2b4a521d36fa5f23c2c9ff0c59e976
Tusharjagi <tusharjagi@gmail.com>
2023-11-07 12:08:44 +0530

    navbar

---
+++ Components/Navbar/Index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid } from "@mui/joy";
+
+import styles from "@/styles/components/Navbar/navbar.module.scss";
+
+import CustomButton from "../common/CustomButton";
+
+const Navbar = (): JSX.Element => {
+  const handle = () => {
+    console.log("Download Resume");
+  };
+  return (
+    <Grid className={styles.navbar_wrapper}>
+      <Grid>
+        <CustomButton text="Resume" onClick={handle} />
+      </Grid>
+      <Grid>
+        <CustomButton text="Resume" onClick={handle} />
+        <CustomButton text="Resume" onClick={handle} />
+        <CustomButton text="Resume" onClick={handle} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Navbar;
