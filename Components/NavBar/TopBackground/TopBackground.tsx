0ac623c5c76ea44059a42a049654b5fa3ad31c50
Tusharjagi <tusharjagi@gmail.com>
2023-11-26 16:42:01 +0530

    Top backgroun component

---
+++ Components/NavBar/TopBackground/TopBackground.tsx
@@ -0,0 +1,7 @@
+import styles from "@/styles/Components/NavBar/TopBackground/topBackground.module.scss";
+
+const TopBackground = () => {
+  return <div className={styles.top_background}></div>;
+};
+
+export default TopBackground;
