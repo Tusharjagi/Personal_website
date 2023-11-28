f5a0fe2faadba1b62f1fde783668f8a8dbadc2e2
Tusharjagi <tusharjagi@gmail.com>
2023-11-28 17:57:11 +0530

    useTypingEffect custom hook

---
+++ hooks/useTypingEffect.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useTextAnimation = (textArray: string[], delay: number = 400) => {
+  const [currentText, setCurrentText] = useState<string>("");
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const currentString: string = textArray[currentIndex % textArray.length];
+      if (currentText.length < currentString.length) {
+        setCurrentText((prevText) => prevText + currentString[currentText.length]);
+      } else {
+        setCurrentText("");
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex, currentText, textArray, delay]);
+
+  return currentText;
+};
+
+export default useTextAnimation;
