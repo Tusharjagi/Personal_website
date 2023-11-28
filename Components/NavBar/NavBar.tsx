75ade1efe9a9b6042466c24194c17f8346d4de30
Tusharjagi <tusharjagi@gmail.com>
2023-11-28 21:55:53 +0530

    Nav bar

---
+++ Components/NavBar/NavBar.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+import { NavbarStyled } from "@/styles/Components/NavBar/NavBarStyled";
+
+import MenuComponent from "./MenuComponent/MenuComponent";
+import NavIcon from "./NavIcon/NavIcon";
+
+const NavBar = (): JSX.Element => {
+  return (
+    <NavbarStyled>
+      <NavIcon />
+      <MenuComponent />
+    </NavbarStyled>
+  );
+};
+
+export default NavBar;
---
+++ styles/Components/NavBar/NavBarStyled.ts
@@ -0,0 +1,8 @@
+import { Grid, styled } from "@mui/joy";
+
+export const NavbarStyled = styled(Grid)`
+  display: flex;
+  justify-content: space-between;
+  padding: 1em;
+  align-items: center;
+`;
