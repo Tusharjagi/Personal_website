87958a88117030fe4571262dc08a90552e364b68
Tusharjagi <tusharjagi@gmail.com>
2023-12-17 00:19:51 +0530

    heading

--- Components/About/About.tsx
+++ Components/About/About.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { AboutStyled } from "@/styles/Components/About/AboutStyled";
+import MovingLine from "../UtilsComponents/MovingLine";
+
 import { nunitoFont } from "@/fonts/googleFonts";
 import { textConsts } from "@/utils/textConst";
 
 const About = () => {
-  return <AboutStyled className={`${nunitoFont.className}`}>{`${textConsts.web} ${textConsts.developer}`}</AboutStyled>;
+  return (
+    <AboutStyled className={`${nunitoFont.className}`}>
+      <span>{`${textConsts.web} ${textConsts.developer}`}</span>
+      <MovingLine margin="0 20% 0 20%" />
+    </AboutStyled>
+  );
 };
 
 export default About;
--- styles/Components/About/AboutStyled.ts
+++ styles/Components/About/AboutStyled.ts
@@ -1,6 +1,12 @@
 import { Grid, styled } from "@mui/joy";
 
 export const AboutStyled = styled(Grid)`
-  display: flex;
+  display: grid;
   gap: 15px;
+  text-align: center;
+  font-size: 120px;
+  font-weight: 600;
+  background: linear-gradient(90deg, var(--bright-gray-color) 3%, var(--lightGray-color) 71%);
+  background-clip: text;
+  text-fill-color: transparent;
 `;
