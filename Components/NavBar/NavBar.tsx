73b62076da7add360350eafdcda254fce21f2857
Tusharjagi <tusharjagi@gmail.com>
2023-12-23 15:12:53 +0530

    nav bar

--- Components/NavBar/NavBar.tsx
+++ Components/NavBar/NavBar.tsx
@@ -5,12 +5,22 @@ import { NavbarStyled } from "@/styles/Components/NavBar/NavBarStyled";
 import MenuComponent from "./MenuComponent/MenuComponent";
 import NavIcon from "./NavIcon/NavIcon";
 
+import { isMobileDevice } from "@/utils/common";
+
+const isMobile = isMobileDevice();
+
 const NavBar = (): JSX.Element => {
   return (
-    <NavbarStyled>
-      <NavIcon />
-      <MenuComponent />
-    </NavbarStyled>
+    <div>
+      {isMobile ? (
+        <div>Mobile </div>
+      ) : (
+        <NavbarStyled>
+          <NavIcon />
+          <MenuComponent />
+        </NavbarStyled>
+      )}
+    </div>
   );
 };
 
