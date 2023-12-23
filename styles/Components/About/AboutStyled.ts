3649333728b789c37c83c541d7293b79167f4ff5
Tusharjagi <tusharjagi@gmail.com>
2023-12-23 16:06:33 +0530

    unit change

--- styles/Components/About/AboutStyled.ts
+++ styles/Components/About/AboutStyled.ts
@@ -3,7 +3,7 @@ import { Grid, styled } from "@mui/joy";
 const AboutStyled = styled(Grid)`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100%;
 `;
 
 export default AboutStyled;
