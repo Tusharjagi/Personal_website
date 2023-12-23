2862b063442144a9f215628ba75057af377e40c6
Tusharjagi <tusharjagi@gmail.com>
2023-12-23 15:51:43 +0530

    add footer for mobile

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -3,13 +3,16 @@ import MovingLine from "../UtilsComponents/MovingLine";
 import style from "@/styles/Components/Footer/bottomBackGround.module.scss";
 
 import { textConsts } from "@/utils/textConst";
+import { isMobileDevice } from "@/utils/common";
+
+const isMobile = isMobileDevice();
 
 const { atSign, copyright, by, tushar } = textConsts;
 
 const Footer = () => {
   return (
     <>
-      <MovingLine margin="0 4%" />
+      <MovingLine margin={isMobile ? "0 6%" : "0 4%"} />
       <div className={style.bottom_background}>
         <FooterStyled>
           <div className="copyright">{`${atSign} ${copyright} ${by} ${tushar}`}</div>
