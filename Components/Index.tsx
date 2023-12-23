d75fea8d0adcc0f2cec9878b493d2e1b3a02234a
Tusharjagi <tusharjagi@gmail.com>
2023-12-23 16:43:14 +0530

    remove

--- Components/Index.tsx
+++ Components/Index.tsx
@@ -5,7 +5,6 @@ import React from "react";
 import NavBar from "./NavBar/NavBar";
 import TopBackground from "./Background/TopBackGround";
 import About from "./About/About";
-import BottomBackGround from "./Background/BottomBackGround";
 import Footer from "./Footer/Footer";
 
 const Index = (): JSX.Element => {
