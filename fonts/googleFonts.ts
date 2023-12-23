97c998df66e22b1cf5f9063910e1319b5acbc5a9
Tusharjagi <tusharjagi@gmail.com>
2023-12-23 18:40:33 +0530

    diphylleia

--- fonts/googleFonts.ts
+++ fonts/googleFonts.ts
@@ -1,4 +1,4 @@
-import { Hind, Nunito } from "next/font/google";
+import { Hind, Nunito, Diphylleia } from "next/font/google";
 
 export const nunitoFont = Nunito({
   subsets: ["latin-ext"],
@@ -8,4 +8,9 @@ export const hind = Hind({
   weight: "500",
   subsets: ["devanagari"],
   preload: true,
+});
+
+export const diphylleia = Diphylleia({
+  weight: "400",
+  subsets: ["latin"],
 });
