0956dd3d6ead5da3e75b0120cbe95e0f88af78a2
Tusharjagi <tusharjagi@gmail.com>
2023-12-23 15:26:13 +0530

    Heading style

--- styles/Components/About/HeadingStyled.ts
+++ styles/Components/About/HeadingStyled.ts
@@ -1,10 +1,14 @@
 import { Grid, styled } from "@mui/joy";
 
-export const HeadingStyled = styled(Grid)`
+type HeadingStyledProps = {
+  isMobile?: boolean;
+};
+
+export const HeadingStyled = styled(Grid)<HeadingStyledProps>`
   display: grid;
   gap: 15px;
   text-align: center;
-  font-size: 120px;
+  font-size: ${(props) => (props.isMobile ? "40px" : "120px")};
   font-weight: 600;
   background: linear-gradient(90deg, var(--bright-gray-color) 3%, var(--lightGray-color) 71%);
   background-clip: text;
