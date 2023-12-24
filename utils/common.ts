87bc0e9fc8f3743bd4687b4906952efd3f1b1133
Tusharjagi <tusharjagi@gmail.com>
2023-12-24 11:46:08 +0530

    update text constants

--- utils/common.ts
+++ utils/common.ts
@@ -1,7 +1,9 @@
 export function isMobileDevice() {
-  const userAgent = window.navigator.userAgent;
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
-    return true;
+  if (typeof window !== undefined) {
+    const userAgent = window.navigator.userAgent;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+      return true;
+    }
+    return false;
   }
-  return false;
 }
