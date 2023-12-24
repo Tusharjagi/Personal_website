dd787dc42368d9b636a3f9b05a5a6282c01b5318
Tusharjagi <tusharjagi@gmail.com>
2023-12-24 11:45:43 +0530

    tech stacks

---
+++ Components/About/TechStack.tsx
@@ -0,0 +1,51 @@
+import { Grid, Typography } from "@mui/joy";
+import TechStackStyled from "@/styles/Components/About/TechStyled";
+import GlassSlab from "../UtilsComponents/GlassSlab";
+import Icons from "../UtilsComponents/Icons";
+
+import { textConsts } from "@/utils/textConst";
+
+const { tech, stacks } = textConsts;
+
+const TechStack = (): JSX.Element => {
+  return (
+    <TechStackStyled>
+      <h2>{`${tech} ${stacks}`}</h2>
+      <GlassSlab width={80}>
+        <Grid className="stack_name">Languages</Grid>
+        <Grid display="flex" gap="2rem" marginBottom="2rem" justifyContent="center">
+          <Icons name="javascript" iconName="js" />
+          <Icons name="typescript" iconName="ts" />
+          <Icons name="html" />
+          <Icons name="css" />
+          <Icons name="sass" />
+          <Icons name="cpp" />
+        </Grid>
+        <Grid className="stack_name">Frameworks & Library</Grid>
+        <Grid display="flex" gap="2rem" marginBottom="2rem" justifyContent="center">
+          <Icons name="react" />
+          <Icons name="angular" />
+          <Icons name="express" />
+          <Icons name="node" />
+          <Icons name="mongodb" />
+        </Grid>
+        <Grid className="stack_name">Version control & platForms</Grid>
+        <Grid display="flex" gap="2rem" marginBottom="2rem" justifyContent="center">
+          <Icons name="git" />
+          <Icons name="github" />
+          <Icons name="gitlab" />
+          <Icons name="docker" />
+          <Icons name="jenkins" />
+        </Grid>
+        <Grid className="stack_name">IDE & Tools</Grid>
+        <Grid display="flex" gap="2rem" justifyContent="center">
+          <Icons name="vs-code" iconName="vscode" />
+          <Icons name="intellij" />
+          <Icons name="postman" />
+        </Grid>
+      </GlassSlab>
+    </TechStackStyled>
+  );
+};
+
+export default TechStack;
---
+++ styles/Components/About/TechStyled.ts
@@ -0,0 +1,44 @@
+import { Grid, styled } from "@mui/joy";
+import { diphylleia } from "@/fonts/googleFonts";
+
+const TechStackStyled = styled(Grid)`
+  margin-bottom: 2rem;
+  .stack_name {
+    position: absolute;
+    left: 0;
+    margin-left: 10%;
+  }
+
+  .stack_name:hover {
+    color: var(--white-color);
+    transform: scale(1.3);
+    transition: all 300ms ease-in-out;
+  }
+
+  .stack_name:not(:hover) {
+    transition: all 300ms ease-in-out;
+  }
+
+  > h2 {
+    padding-left: 8rem;
+    padding-bottom: 0.5rem;
+    font-family: ${diphylleia};
+    color: var(--lightGray-color);
+  }
+
+  > h2:hover {
+    color: var(--white-color);
+    transform: scale(1.1);
+    transition: all 300ms ease-in-out;
+  }
+
+  > h2:not(:hover) {
+    transition: all 300ms ease-in-out;
+  }
+
+  > div > div {
+    justify-content: center;
+  }
+`;
+
+export default TechStackStyled;
