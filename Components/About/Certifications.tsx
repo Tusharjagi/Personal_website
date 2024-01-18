e226839810bc05532bfdf773716a59d3d41d34f0
TusharJagi <tushar.j@fnp.com>
2024-01-18 17:36:45 +0530

    certifications comp

--- Components/About/Certifications.tsx
+++ Components/About/Certifications.tsx
@@ -1,5 +1,19 @@
+import CertificationStyled from "@/styles/Components/About/CertificationStyled";
+import { textConsts } from "@/utils/textConst";
+import GlassSlab from "../UtilsComponents/GlassSlab";
+import { Grid } from "@mui/joy";
+
 const Certifications = (): JSX.Element => {
-  return <h1>Certificates</h1>;
+  return (
+    <CertificationStyled>
+      <h2>{textConsts.certifications}</h2>
+      <GlassSlab width={80}>
+        <Grid>
+          <Grid>Meta front End Developer</Grid>
+        </Grid>
+      </GlassSlab>
+    </CertificationStyled>
+  );
 };
 
 export default Certifications;
