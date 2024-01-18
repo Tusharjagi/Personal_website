977f5521d5b3a281a6e6866ccf676694f4ac3c75
TusharJagi <tushar.j@fnp.com>
2024-01-18 17:41:41 +0530

    project styled

---
+++ styles/Components/About/ProjectStyled.ts
@@ -0,0 +1,26 @@
+import { Grid, styled } from "@mui/joy";
+import { diphylleia } from "@/fonts/googleFonts";
+
+const ProjectsStyled = styled(Grid)`
+  margin-bottom: 2rem;
+
+  > h2 {
+    display: inline-flex;
+    padding: 0 2rem 0.5rem 8rem;
+    font-family: ${diphylleia};
+    color: var(--lightGray-color);
+  }
+
+  > h2:hover {
+    color: var(--white-color);
+    transform: scale(1.1);
+    transform: translateX(-20%);
+    transition: all 300ms ease-in-out;
+  }
+
+  > h2:not(:hover) {
+    transition: all 300ms ease-in-out;
+  }
+`;
+
+export default ProjectsStyled;
