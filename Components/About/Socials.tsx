6b393c8ffc56e5dffc33ac70c2f4f5ca3bb2d9a8
TusharJagi <tushar.j@fnp.com>
2024-01-22 21:01:44 +0530

    socials

--- Components/About/Socials.tsx
+++ Components/About/Socials.tsx
@@ -2,6 +2,7 @@ import SocialStyled from "@/styles/Components/About/SocialStyled";
 import GlassSlab from "../UtilsComponents/GlassSlab";
 import { Grid } from "@mui/joy";
 import { textConsts } from "@/utils/textConst";
+import Icons from "../UtilsComponents/Icons";
 
 const Socials = (): JSX.Element => {
   const { Socials } = textConsts;
@@ -10,7 +11,23 @@ const Socials = (): JSX.Element => {
     <SocialStyled>
       <h2>{Socials}</h2>
       <GlassSlab width={80}>
-        <Grid>Socials links</Grid>
+        <Grid className="social_icons">
+          <a href="https://www.linkedin.com/in/tushar-jagi-519025215" target="_blank" rel="noopener noreferrer">
+            <Icons name="linkedin" />
+          </a>
+          <a href="https://github.com/Tusharjagi" target="_blank" rel="noopener noreferrer">
+            <Icons name="github" />
+          </a>
+          <a href="mailto:tusharjagi@gmail.com" target="_blank" rel="noopener noreferrer">
+            <Icons name="gmail" />
+          </a>
+          <a href="https://medium.com/@tusharjagi" target="_blank" rel="noopener noreferrer">
+            <Icons name="medium" />
+          </a>
+          <a href="https://tusharjagi.hashnode.dev" target="_blank" rel="noopener noreferrer">
+            <Icons name="hashNode" />
+          </a>
+        </Grid>
       </GlassSlab>
     </SocialStyled>
   );
--- styles/Components/About/SocialStyled.ts
+++ styles/Components/About/SocialStyled.ts
@@ -4,6 +4,24 @@ import { diphylleia } from "@/fonts/googleFonts";
 const SocialStyled = styled(Grid)`
   margin-bottom: 2rem;
 
+  .social_icons {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 16px;
+    flex-wrap: wrap;
+
+    @media (max-width: 600px) {
+      flex-direction: column;
+      align-items: center;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+      cursor: pointer;
+    }
+  }
+
   > h2 {
     display: inline-flex;
     padding: 0 2rem 0.5rem 8rem;
