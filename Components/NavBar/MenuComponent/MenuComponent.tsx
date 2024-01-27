230da86489ff6a6ec317da8f66e3a769cce6502c
TusharJagi <tushar.j@fnp.com>
2024-01-27 17:47:28 +0530

    new menu buttons

--- Components/NavBar/MenuComponent/MenuComponent.tsx
+++ Components/NavBar/MenuComponent/MenuComponent.tsx
@@ -3,16 +3,51 @@ import { MenuStyled } from "@/styles/Components/NavBar/MenuComponent/MenuStyled"
 import CustomButton from "@/components/CustomButton/CustomButton";
 
 const MenuComponent = (): JSX.Element => {
-  const handleHome = () => {
-    console.log("HOME");
+  const scrollToSection = (sectionId: string): void => {
+    const section = document.getElementById(sectionId);
+    if (section !== null) {
+      section.scrollIntoView({ behavior: "smooth" });
+    } else {
+      console.error("Section not found");
+    }
   };
 
   return (
     <MenuStyled>
-      <CustomButton text="HOME" onClick={handleHome} />
-      <CustomButton text="ABOUT" onClick={handleHome} />
-      <CustomButton text="PROJECT" onClick={handleHome} />
-      <CustomButton text="CONTACT" onClick={handleHome} />
+      <CustomButton
+        text="TECH STACK"
+        onClick={() => {
+          scrollToSection("stack");
+        }}
+      />
+
+      <CustomButton
+        text="CARRIER"
+        onClick={() => {
+          scrollToSection("carrier");
+        }}
+      />
+
+      <CustomButton
+        text="CERTIFICATES"
+        onClick={() => {
+          scrollToSection("certificate");
+        }}
+      />
+
+      <CustomButton
+        text="PROJECT"
+        onClick={() => {
+          scrollToSection("project");
+        }}
+      />
+      <CustomButton
+        style={{ padding: "0 20px" }}
+        icon="github.svg"
+        onClick={() => {
+          scrollToSection("contact");
+        }}
+      />
     </MenuStyled>
   );
 };
