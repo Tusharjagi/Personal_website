28d15723df8503bc69146b8f03093377c671e400
TusharJagi <tushar.j@fnp.com>
2024-01-27 17:47:08 +0530

    add new funcationaliy

--- Components/CustomButton/CustomButton.tsx
+++ Components/CustomButton/CustomButton.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 import styles from "@/styles/Components/CustomButton/CustomButton.module.scss";
+import Image from "next/image";
 
-type customButtonProps = {
-  text: string;
+interface customButtonProps {
+  text?: string;
   onClick: () => void;
-};
+  icon?: string;
+  style?: React.CSSProperties;
+}
 
-const CustomButton = ({ text, onClick }: customButtonProps): JSX.Element => {
+const CustomButton = ({ text, onClick, icon, style }: customButtonProps): JSX.Element => {
   return (
-    <button onClick={onClick} className={styles.button}>
-      {text}
+    <button onClick={onClick} className={styles.button} style={style}>
+      {icon !== undefined ? <Image src={`/assets/icons/${icon}`} width={30} height={30} alt={`${icon}`}></Image> : text}
       <span />
     </button>
   );
